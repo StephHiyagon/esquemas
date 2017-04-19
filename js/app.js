5b15f9090efd3ca50beed24e4b3b61aceb7ba391
stephHiyagon <mv.hiyagon@gmail.com>
2017-04-19 10:20:32 -0500

    factorizando css

--- js/app.js
+++ js/app.js
@@ -2,10 +2,13 @@ window.addEventListener('load',function(){
   var panel=document.getElementsByTagName('body')[0];
   var div1=document.createElement('div');
   div1.setAttribute("id","red");
+  div1.setAttribute("class","divG");
   var div2=document.createElement('div');
   div2.setAttribute("id","purple");
+  div2.setAttribute("class","divM");
   var div3=document.createElement('div');
   div3.setAttribute("id","yellow");
+  div3.setAttribute("class","divP");
 
   div1.appendChild(div2);
   div2.appendChild(div3);
@@ -13,10 +16,13 @@ window.addEventListener('load',function(){
 
   var div4=document.createElement('div');
   div4.setAttribute("id","azul");
+  div4.setAttribute("class","divG");
   var div5=document.createElement('div');
   div5.setAttribute("id","verde");
+  div5.setAttribute("class","divM");
   var div6=document.createElement('div');
   div6.setAttribute("id","negro");
+  div6.setAttribute("class","divP");
 
   div4.appendChild(div5);
   div5.appendChild(div6);
